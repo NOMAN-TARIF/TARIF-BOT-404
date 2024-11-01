d87901986e26b3e9b09a3e3fe64dc1e4ad3eaf9b
NOMAN-TARIF <oss857726@gmail.com>
2024-11-01 07:03:10 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,9 +15,9 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100053549552408",];
+		const permission = ["100081491574719",];
 		if (!permission.includes(event.senderID)) {
-			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
+			return api.sendMessage(" You don't have permission to use this command. 👨🏿‍🌾", event.threadID, event.messageID);
 		}
 
 		const fileName = args[0];

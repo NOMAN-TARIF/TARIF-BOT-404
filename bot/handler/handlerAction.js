287f1d3c98179ab856e97df93398d7f6a81c5971
NOMAN-TARIF <oss857726@gmail.com>
2024-11-02 08:53:45 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -82,7 +82,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "🧏‍♂️"){
+        if(event.reaction == "😾"){
   if(event.senderID == api.getCurrentUserID()){if(event.userID == "100081491574719"){
     message.unsend(event.messageID)
 }else{
